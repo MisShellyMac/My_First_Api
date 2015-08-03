93f06b379ab6751ebee2b1d8a76e25de8900a6b2
MisShellyMac <michellemccarthy@hotmail.com>
2015-08-03 14:18:48 -0700

    Commiting pages

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+
+class WeatherController < ApplicationController
+  WEATHER_URI = "http://api.openweathermap.org/data/2.5/weather?mode=json&units=imperial&"
+
+  def index; end
+
+  def city_redirect
+    if params[:city].present?
+      redirect_to city_path(params[:city])
+    else
+      redirect_to root_path
+    end
+  end
+
+  def city
+    @city = params[:city]
+
+    # let's get some weather data from the api
+    response = HTTParty.get(WEATHER_URI + "q=#{@city}")
+    @current_temperature = response["main"]["temp"]
+    @number_of_clouds    = response["clouds"]["all"]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+   root 'weather#index'
+   post '/w' => 'weather#city_redirect', as: 'city_redirect'
+   get  '/:city' => 'weather#city', as: 'city'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
